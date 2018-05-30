066810145d942ceaeb8d615ca22a4c8de15252f6
sammwaura <meshsami27@gmail.com>
2018-05-30 17:43:13 +0300

    add style

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,22 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
+
+
 public class App {
   public static void main(String[] args) {
+
+    
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+        port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+        port = 4567;
+    }
+  
+    setPort(port);
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
